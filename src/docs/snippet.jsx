54ffa72c6e3edf3401e41a5a862264d1ad74d632
Clay Gregory <clay@claygregory.com>
2018-04-14 03:04:31 -0500

    move to prism for doc syntax highlighting

--- src/docs/snippet.jsx
+++ src/docs/snippet.jsx
@@ -1,6 +1,9 @@
 
 import React from 'react';
-import Highlight from 'react-highlight';
+import SyntaxHighlighter, { registerLanguage } from 'react-syntax-highlighter/prism-light';
+import jsx from 'react-syntax-highlighter/languages/prism/jsx';
+import style from 'react-syntax-highlighter/styles/prism/atom-dark';
+
 
 const START_TAG = '/* ---START SNIPPET--- */';
 const END_TAG = '/* ---END SNIPPET--- */';
@@ -10,8 +13,10 @@ const Snippet = ({ source }) => {
   const start = source.indexOf(START_TAG) + START_TAG.length + 1;
   const end = source.indexOf(END_TAG);
 
+  registerLanguage('jsx', jsx);
+
   return (
-    <Highlight className="javascript">{source.slice(start, end)}</Highlight>
+    <SyntaxHighlighter useInlineStyles language="jsx" style={style}>{source.slice(start, end)}</SyntaxHighlighter>
   );
 
 };
